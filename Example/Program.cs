112239edfb44bad38888a7f74a97bafa27b6b922
Nikita Kireev <kireevinvest@yandex.ru>
2022-05-15 17:39:30 +0300

    update example

--- Example/Program.cs
+++ Example/Program.cs
@@ -8,3 +8,8 @@ int[] array2=new int[int.Parse(Console.ReadLine())].Select(x=>new Random().Next(
 //Console.WriteLine(String.Join(" ",array));
 //Console.WriteLine(String.Join(" ",array1));
 Console.WriteLine(String.Join(" ",array2));
+
+/* 
+int[] k=new int[20].Select(x=>new Random().Next(0,1000)).Where(x=>x>=9&&x<=100).ToArray();
+Console.WriteLine(String.Join(" ",k));
+*/
