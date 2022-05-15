48b10c1a00a83a540d7bce5bd434846b51cc4377
Nikita Kireev <kireevinvest@yandex.ru>
2022-05-15 21:24:22 +0300

    task42

---
+++ Task42/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+Console.Clear();
+
+/*
+**Задача 42:** Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+45 -> 101101
+
+3 -> 11
+
+2 -> 10
+*/
+
+Console.WriteLine("Введите число: ");
+int numbers = int.Parse(Console.ReadLine());
+
+string s = string.Empty;
+
+while (numbers > 0)
+{
+    s = numbers % 2 + s;
+    numbers = numbers / 2;
+}
+Console.WriteLine(s);
