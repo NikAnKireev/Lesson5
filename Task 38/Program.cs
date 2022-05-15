4b9c6131ef6f9d05371b4b04ae64b2fe2736affd
Nikita Kireev <kireevinvest@yandex.ru>
2022-05-15 18:33:43 +0300

    task38

---
+++ Task 38/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+Console.Clear();
+
+/* Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76 */
+
+int[] array = new int[6].Select(x => new Random().Next(0, 100)).ToArray();
+Console.WriteLine(string.Join(" ", array));
+
+int max = array[0];
+int min = array[0];
+int result = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (max < array[i])
+    {
+        max = array[i];
+    }
+    if (min > array[i])
+    {
+        min = array[i];
+    }
+    result = max - min;
+}
+Console.WriteLine($"{max} - {min} = {result}");
