8518f8779121e0eae73e298ca5e2c069a60f7678
Nikita Kireev <kireevinvest@yandex.ru>
2022-05-15 19:35:01 +0300

    task36

---
+++ Task36/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+Console.Clear();
+
+/* Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 26
+
+[-4, -6, 89, 6] -> 85 */
+
+int[] array = new int[5].Select(x => new Random().Next(-100, 100)).ToArray();
+
+int summa = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 == 1)
+    {
+        summa = summa + array[i + 1];
+    }
+}
+Console.WriteLine($"{String.Join(" ", array)} -> {summa}");
