f82be5cfd150029f8f56f40cb6d4139c662b2af6
Nikita Kireev <kireevinvest@yandex.ru>
2022-05-15 16:45:34 +0300

    Task35

---
+++ Task35/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+Console.Clear();
+
+/* **Задача 35:** Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, 
+значения которых лежат в отрезке [10,99].
+
+*Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123*
+
+[5, 18, 123, 6, 2] -> 1
+
+[1, 2, 3, 6, 2] -> 0
+
+[10, 11, 12, 13, 14] -> 5 */
+
+int[] array = new int[int.Parse(Console.ReadLine())].Select(x => new Random().Next(0, 1000)).ToArray();
+Console.WriteLine(String.Join(" ", array));
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] >= 10 && array[i]<= 99)
+    count++;
+}
+Console.WriteLine($"Количество: {count}");
