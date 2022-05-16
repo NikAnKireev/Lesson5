5e329d751cfdd44661a00df5e006a59dd471fe89
Nikita Kireev <kireevinvest@yandex.ru>
2022-05-16 17:31:28 +0300

    task44, 45

---
+++ Task44/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+Console.Clear();
+
+/*
+**Задача 44:** Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+Если N = 5 -> 0 1 1 2 3
+
+Если N = 3 -> 0 1 1
+
+Если N = 7 -> 0 1 1 2 3 5 8
+*/
+
+int a1 = int.Parse(Console.ReadLine());
+int a2 = int.Parse(Console.ReadLine());
+int a3 = int.Parse(Console.ReadLine());
+int[] fib = new int[a3];
+fib[0] = a1;
+fib[1] = a2;
+
+for (int i = 2; i < fib.Length; i++)
+{
+    fib[i] = fib[i - 1] + fib[i - 2];
+}
+Console.WriteLine(string.Join(" ", fib));
---
+++ Task45/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+Console.Clear();
+
+/*
+Задача 45: Напишите программу, которая будет создавать копию заданного массива 
+с помощью поэлементного копирования.
+*/
+
+int[] array = new int[5].Select(x => x = new Random().Next(0, 10)).ToArray();
+Console.WriteLine(String.Join(" ", array));
+
+int[] copyArray(int[] arr)
+{
+    int[] newArray = new int[arr.Length];
+    for (int i = 0; i < newArray.Length; i++)
+        newArray[i] = arr[i];
+    return newArray;
+
+}
+Console.WriteLine(String.Join(" ", copyArray(array)));
